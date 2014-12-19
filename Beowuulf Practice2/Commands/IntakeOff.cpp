cb744bcb98f7b73163dd1a261efae6c7f1b82cf5
KillerAsuna <writingbloc99@gmail.com>
2014-12-19 14:11:55 -0800

    IntakeOn and IntakeOff operational.

--- Beowuulf Practice2/Commands/IntakeOff.cpp
+++ Beowuulf Practice2/Commands/IntakeOff.cpp
@@ -26,7 +26,7 @@ void IntakeOff::Initialize() {
 
 // Called repeatedly when this Command is scheduled to run
 void IntakeOff::Execute() {
-	
+	Robot::intake->intakeMotor->Set(0.0);
 }
 
 // Make this return true when this Command no longer needs to run execute()
